3f49d2c37bbcbfd04c9e409271cf7f3195b528c4
andreyudi10 <andrianus.yudi@gmail.com>
2020-10-30 11:38:42 +0700

    controled state adding

--- React.jsx
+++ React.jsx
@@ -181,3 +181,35 @@ class Counter extends React.Component {
     }
   };
   
+//   controled state
+//When you type in the input box, that text is processed by the handleChange() method, set as the input property in the local state, and rendered as the value in the input box on the page. The component state is the single source of truth regarding the input data.
+// Last but not least, don't forget to add the necessary bindings in the constructor.
+
+class ControlledInput extends React.Component {
+    constructor(props) {
+       super(props);
+       this.state = {
+         input: ''
+       };
+       // change code below this line
+       this.handleChange = this.handleChange.bind(this);
+       // change code above this line
+     }
+     // change code below this line
+     handleChange(event){
+       this.setState(      
+         {input:event.target.value});
+     }
+     // change code above this line
+     render() {
+       return (
+         <div>
+           { /* change code below this line */}
+           <input type="text" value={this.state.input} onChange={this.handleChange} />
+           { /* change code above this line */}
+           <h4>Controlled Input:</h4>
+           <p>{this.state.input}</p>
+         </div>
+       );
+     }
+   };
