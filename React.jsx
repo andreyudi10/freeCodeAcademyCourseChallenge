a8b111f78c2ee135d7c6c768c9b54b404994f4a1
andreyudi10 <andrianus.yudi@gmail.com>
2020-11-04 11:24:50 +0700

    update terbaru

--- React.jsx
+++ React.jsx
@@ -606,8 +606,172 @@ class MyComponent extends React.Component {
   }
   //      <div>
   //        <button onClick={this.toggleDisplay}>Toggle Display</button>         
-  //        {this.state.display?<h1>Displayed!</h1>:""}   
   //      </div>
   //   );
   // }
+};
+
+// if else with more concise way
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: true
+    }
+    this.toggleDisplay = this.toggleDisplay.bind(this);
+  }
+  toggleDisplay() {
+    this.setState(state => ({
+      display: !state.display
+    }));
+  }
+  render() {
+    // Change code below this line
+    return (
+       <div>
+         <button onClick={this.toggleDisplay}>Toggle Display</button>
+         {this.state.display && <h1>Displayed!</h1>}
+       </div>
+    );
+  }
+};
+
+
+const inputStyle = {
+  width: 235,
+  margin: 5
+};
+
+// React: Use a Ternary Expression for Conditional Rendering
+class CheckUserAge extends React.Component {
+  constructor(props) {
+    super(props);
+    // Change code below this line
+    this.state={
+      input:"",
+      userAge:""
+    }
+    
+    // Change code above this line
+    this.submit = this.submit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      input: e.target.value,
+      userAge: ''
+    });
+  }
+  submit() {
+    this.setState(state => ({
+      userAge: state.input
+    }));
+  }
+  render() {
+    const buttonOne = <button onClick={this.submit}>Submit</button>;
+    const buttonTwo = <button>You May Enter</button>;
+    const buttonThree = <button>You Shall Not Pass</button>;
+    return (
+      <div>
+        <h3>Enter Your Age to Continue</h3>
+        <input
+          style={inputStyle}
+          type='number'
+          value={this.state.input}
+          onChange={this.handleChange}
+        />
+        <br />
+        {/* Change code below this line */}
+        {
+          this.state.input && this.state.userAge?
+          this.state.userAge>=18?
+          buttonTwo:
+          buttonThree:
+          buttonOne
+        }
+
+        {/* Change code above this line */}
+      </div>
+    );
+  }
+}
+
+// React: Render Conditionally from Props
+// Now you have an expression that you can use to make a randomized decision in the code. Next you need to implement this.
+
+class Results extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    {/* Change code below this line */}
+    return <h1>{this.props.fiftyFifty>=0.5?"You win":"You lose"}</h1>;
+    {/* Change code above this line */}
+  }
+}
+
+class GameOfChance extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 1
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState(
+      state =>(
+      {
+      counter: state.counter+1 // Change this line
+    }));
+  }
+  render() {
+    const expression = Math.random(); // Change this line
+    return (
+      <div>
+        <button onClick={this.handleClick}>Play Again</button>
+        {/* Change code below this line */}
+        <Results fiftyFifty={expression}/>        
+
+        {/* Change code above this line */}
+        <p>{'Turn: ' + this.state.counter}</p>
+      </div>
+    );
+  }
+}
+
+// React: Change Inline CSS Conditionally Based on Component State
+class GateKeeper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ input: event.target.value })
+  }
+  render() {
+    let inputStyle = {
+      border: '1px solid black',      
+    };
+    // Change code below this line
+    if(this.state.input.length>15){
+      inputStyle.border='3px solid red'
+    }    
+    
+
+    // Change code above this line
+    return (
+      <div>
+        <h3>Don't Type Too Much:</h3>
+        <input
+          type="text"
+          style={inputStyle}          
+          value={this.state.input}
+          onChange={this.handleChange} />
+      </div>
+    );
+  }
 };
